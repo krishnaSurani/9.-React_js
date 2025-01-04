6568b41e24c79014597eb4ad834285f9ef9fe7f8
KrishnaSurani <krishnasurani07@gmail.com>
2025-01-04 21:30:15 +0530

    day-1

---
+++ Day-1/src/projects.jsx
@@ -0,0 +1,16 @@
+
+function Projects()
+{
+    return (
+        <>
+        <h4>Projects</h4>
+        
+            <h5>1. Portfolio Website</h5>
+            <p className='para2'>A personal portfolio website showcasing my skill and projects</p>
+            <h5>2. Task Management App.</h5>
+            <p className='para2'>An interactive web app for managing tasks and to-do lists.</p>
+        </>
+    )
+}
+
+export default Projects
---
+++ Day-1/src/skill.jsx
@@ -0,0 +1,17 @@
+function Skill ()
+{
+    return (
+       <div>
+        <h2>Skills</h2>
+        <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Javascript</li>
+            <li>Responsive Web Design</li>
+            <li>Frontend Framework (e.g., React)</li>
+        </ul>
+       </div>
+    )
+}
+
+export default Skill
