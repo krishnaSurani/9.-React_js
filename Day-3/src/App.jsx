c28c46b42af3286e4c7c34f19e6475350262c42b
KrishnaSurani <krishnasurani07@gmail.com>
2025-01-04 21:29:06 +0530

    day-3

---
+++ Day-3/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Counter from './component/counter'
+import Counter2 from './component/counter2'
+
+function App() {
+
+  return (
+    <>
+      <Counter></Counter>
+      <Counter2></Counter2>
+    </>
+  )
+}
+
+export default App
